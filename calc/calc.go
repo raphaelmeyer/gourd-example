e3c2771cd1c372d4691679008d5352343ee7471c
Raphael Meyer <raphael.meyer@bbv.ch>
2015-04-03 17:17:18 +0200

    Update example to more or less match example from cucumber page

--- calc/calc.go
+++ calc/calc.go
@@ -2,3 +2,13 @@ package calc
 
 type Calculator struct{}
 
+func (calculator *Calculator) Push(number int) {
+}
+
+func (calculator *Calculator) Add() {
+}
+
+func (calculator *Calculator) Result() int {
+	return 0
+}
+
--- features/step_definitions/steps.go
+++ features/step_definitions/steps.go
@@ -5,22 +5,36 @@ import (
 	"github.com/raphaelmeyer/gourd-example/calc"
 )
 
-type scenario struct {
+type calc_context struct {
 	calc calc.Calculator
 }
 
 func new_scenario() interface{} {
-	return &scenario{}
+	return &calc_context{}
 }
 
 func main() {
 	cucumber := gourd.NewCucumber(new_scenario)
 
-	cucumber.Given("^I have entered (\\d+) into the calculator$").Pass()
+	cucumber.Given("^I have entered (\\d+) into the calculator$").Do(
+		func(context interface{}, args ...interface{}) {
+			scenario, _ := context.(*calc_context)
+			scenario.calc.Push(50)
+		})
 
-	cucumber.When("^I press add$").Pass()
+	cucumber.When("^I press add$").Do(
+		func(context interface{}, args ...interface{}) {
+			scenario, _ := context.(*calc_context)
+			scenario.calc.Add()
+		})
 
-	cucumber.Then("^the result should be (\\d+) on the screen$").Pass()
+	cucumber.Then("^the result should be (\\d+) on the screen$").Do(
+		func(context interface{}, args ...interface{}) {
+			scenario, _ := context.(*calc_context)
+			if scenario.calc.Result() != 120 {
+				panic("Wrong result")
+			}
+		})
 
 	cucumber.Run()
 }
