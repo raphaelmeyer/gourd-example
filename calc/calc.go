02de2f3bffab27e8a4b95a50b52df352de0634a6
raphael meyer <raphael.akira.meyer@gmail.com>
2015-04-05 14:22:49 +0200

    implement example

--- calc/calc.go
+++ calc/calc.go
@@ -1,14 +1,18 @@
 package calc
 
-type Calculator struct{}
-
-func (calculator *Calculator) Push(number int) {
+type Calculator struct {
+	stack []int
 }
 
-func (calculator *Calculator) Add() {
+func (c *Calculator) Push(number int) {
+	c.stack = append(c.stack, number)
 }
 
-func (calculator *Calculator) Result() int {
-	return 0
+func (c *Calculator) Add() {
+	last := len(c.stack) - 1
+	c.stack = append(c.stack[:last-1], c.stack[last-1]+c.stack[last])
 }
 
+func (c *Calculator) Result() int {
+	return c.stack[len(c.stack)-1]
+}
